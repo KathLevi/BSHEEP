785cb1deb7b5bb501bb2c01db7a7ff2462c708a4
KathLevi <levi.kathleen1@gmail.com>
2016-12-13 16:26:14 -0800

    Updates that I forgot about

--- BSHEEP/Program.cs
+++ BSHEEP/Program.cs
@@ -10,6 +10,8 @@ namespace BSHEEP
         public int X;
         public int Y;
         public int index;
+        public double angle;        //angle with respect to the first point (p0)
+        public double distance;     //distance away from the first point (p0)
     }
 
     class Program
@@ -70,11 +72,11 @@ namespace BSHEEP
         private static int orientation(Point p, Point q, Point r)
         {
             int val = (q.Y - p.Y) * (r.X - q.X) - (q.X - p.X) * (r.Y - q.Y);
-            if (val == 0) return 0; // collinear
+            if (val == 0) return 0;     // collinear
             return (val > 0) ? 1 : 2;   //clock or counter clockwise
         }
 
-        //sort an array of points with respect to the first point
+//sort an array of points with respect to the first point
 
         private static double RunConvexHull()
        {
@@ -85,26 +87,41 @@ namespace BSHEEP
             //Place bottom-most point at first position
             borderSheep.Add(bottomLeft);
 
-            //Sort n-1 points with respect to the first point
-            //A point p1 comes before p2 if p2 has a larger polar angle than p1
+//Sort n-1 points with respect to the first point
+//A point p1 comes before p2 if p2 has a larger polar angle than p1
+
 
             //if 2 or more points make the same angle with p0,
             //remove all but the one that is farthest from p0
-            
-            //if modified array of points has less than 3 points,
-            //convex hull is not possible
-
-            //create empty stack and push first 3 points to it
-
-            //process remaining n-3 points
-
-            //Now stack has the output points, print contents of stack
-
-
-            sheep.ForEach(s =>
+            int size = sheep.Count();
+            for (int i = 0; i < size; i++)     //go through array 
             {
+                for (int j = 0; j < size; j++) //compare to every element in an array
+                {
+                     if (sheep[i].angle == sheep[j].angle)
+                    {
+                        if (sheep[i].distance > sheep[j].distance)
+                            sheep.RemoveAt(j);
+                        else
+                            sheep.RemoveAt(i);
+                        size--;
+                    }
+                }
+            }           
+           
 
-            });
+//create empty stack and push first 3 points to it
+//process remaining n-3 points
+//Now stack has the output points
+           
+                 
+            //if list of border sheep of points has less than 3 points,
+            //convex hull is not possible,
+            if (borderSheep.Count() < 3)
+            {
+                borderSheep.Clear();    //clear border sheep so that we do not accidentally output values
+                circ = 0;               //set circ to 0 since convex hull not posisble
+            }
 
             return circ;
         }
